f91d0bc39ceb6d9c8665a23559312432941cc88a
Allyn Bao <baoallyn@gmail.com>
2023-02-02 09:46:30 -0500

    Initial commit

---
+++ analyze.c
@@ -0,0 +1,28 @@
+#include "analyze.h"
+
+// You can add other functions if you want to perform
+// other calculations indepenent of these functions.
+
+void data_init( data_t *p_this, double step_size ) {
+    // Your implementation here...
+}
+
+// You may not need this if you do not have a dynamically
+// allocated array.
+void data_destroy( data_t *p_this ) {
+    // Your implementation here...
+}
+
+void data_append( data_t *p_this, double new_value ) {
+    // Your implementation here...
+}
+
+double data_current( data_t *p_this ) {
+    // Your implementation here...
+    return 0.0;
+}
+
+double data_next( data_t *p_this ) {
+    // Your implementation here...
+    return 0.0;
+}
